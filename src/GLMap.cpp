00eae6a617436fa2c1721b29ffdfc4159a2aaf14
jm <jm@9a1b16dc-701e-0410-8b73-93c416b5d9a1>
2002-01-02 05:50:32 +0000

    Stuff
    git-svn-id: svn://svn.icculus.org/bitstream/trunk@89 9a1b16dc-701e-0410-8b73-93c416b5d9a1

--- src/GLMap.cpp
+++ src/GLMap.cpp
@@ -134,13 +134,13 @@ GLMap::draw()const{
 
       glColor3f(1.0f, 1.0f, 1.0f);
       glNormal3f(0.0f, -1.0f, 0.0f);
-      glTexCoord2f(0.0f+dZ/800, 0.0f+dZ/50);
+      glTexCoord2f(0.0f+dZ/800, 0.0f+dZ/100);
       glVertex3f(-150.0f, 40.0f, 0.0f);
-      glTexCoord2f(0.0f+dZ/800, 1.0f+dZ/50);
+      glTexCoord2f(0.0f+dZ/800, 1.0f+dZ/100);
       glVertex3f(-150.0f, -5.0f, -95.0f);
-      glTexCoord2f(1.0f+dZ/800, 1.0f+dZ/50);
+      glTexCoord2f(1.0f+dZ/800, 1.0f+dZ/100);
       glVertex3f(150.0f, -5.0f, -95.0f);
-      glTexCoord2f(1.0f+dZ/800, 0.0f+dZ/50);
+      glTexCoord2f(1.0f+dZ/800, 0.0f+dZ/100);
       glVertex3f(150.0f, 40.0f, 0.0f);
 
       glEnd();
