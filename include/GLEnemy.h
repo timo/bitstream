eee6b95d73b2bac4bba34c8cf48c77b7ab42d890
jm <jm@9a1b16dc-701e-0410-8b73-93c416b5d9a1>
2002-01-04 06:43:23 +0000

    Prelim enemy code and model
    git-svn-id: svn://svn.icculus.org/bitstream/trunk@94 9a1b16dc-701e-0410-8b73-93c416b5d9a1

---
+++ include/GLEnemy.h
@@ -0,0 +1,80 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: t; c-basic-offset: 3 -*- */
+
+/*
+   Bitstream:  An OpenGL Space Action Game
+   http://icculus.org/bitstream/
+
+   Copyright (C) 2001, 2002 Jeff Mrochuk
+   
+   This file is part of bitstream.     Bitstream is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.     
+   
+   Bitstream is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     
+   You should have received a copy of the GNU General Public License along with Bitstream; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Jeff Mrochuk
+jm@icculus.org
+
+*/
+
+///////////////////////////////////////////////////////////////////////////
+// Name: GLEnemy 
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+// Modifications:
+//
+//
+///////////////////////////////////////////////////////////////////////////
+#ifndef _GLEnemy_h_
+#define _GLEnemy_h_
+
+// System Includes
+//
+
+// Project Includes
+//
+
+// Local Includes
+//
+
+// Forward References
+//
+
+/**   
+  *    @author 
+  *    @date 
+  */
+class GLEnemy
+{
+public:
+
+// Lifecycle
+
+   GLEnemy();
+   GLEnemy(const GLEnemy&);            // copy constructor
+   ~GLEnemy();
+
+// Operator
+   
+   GLEnemy&   operator=(const GLEnemy&);     // assignment operator
+
+// Operations
+
+// Access
+
+// Inquiry
+
+protected:
+// Protected Methods
+private:
+// Private Methods
+
+
+//////////////////Removed
+};
+
+// Inline Methods
+//
+// External References
+//
+
+#endif  // _GLEnemy_h
