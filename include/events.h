625dddaa7aae1d48135dfa4b69ad34ae487aadb0
jm <jm@9a1b16dc-701e-0410-8b73-93c416b5d9a1>
2003-01-07 01:47:15 +0000

    Yar
    git-svn-id: svn://svn.icculus.org/bitstream/trunk@167 9a1b16dc-701e-0410-8b73-93c416b5d9a1

---
+++ include/events.h
@@ -0,0 +1,8 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: t; c-basic-offset: 3 -*- */
+
+#ifndef _events_h_
+#define _events_h_
+
+ int attempt_fullscreen_toggle(SDL_Surface **surface, Uint32 *flags);
+
+#endif // _events_h_
